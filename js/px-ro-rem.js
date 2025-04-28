f618d415fc0c61e86985bb08fb9e03d80582a58d
David Loop <davidloop@gmail.com>
2025-04-27 22:04:31 -0500

    Initial commit

---
+++ js/px-ro-rem.js
@@ -0,0 +1,88 @@
+(function($) {
+
+    function initForms() {
+        var inputs = $("input");
+
+        inputs.each(function() {
+            var $input = $(this);
+            $input.attr('data-placeholder', $input.attr('placeholder'));
+        });
+
+        inputs.on('focus', function() {
+            $(this).attr('placeholder', '');
+        }).on('blur', function() {
+            $(this).attr('placeholder', $(this).attr('data-placeholder'));
+        });
+    }
+
+    initForms();
+    
+
+    const $base = $("[id=baseFontSize]");
+    const $pixels = $("[id=pxValue]");
+    const $rems = $("[id=remValue]");
+    let lastEdited = 'px';
+
+    function pxToRem(px, base) {
+        return (px / base).toFixed(4);
+    }
+
+    function remToPx(rem, base) {
+        return (rem * base).toFixed(2);
+    }
+
+    function trimZeros(num) {
+        num = parseFloat(num);
+        return isNaN(num) ? '' : num.toFixed(4).replace(/\.?0+$/, '');
+    }
+
+    $base.on('keyup', function() {
+        const base = parseFloat($(this).val()) || 16;
+
+        if (lastEdited === 'px') {
+            const px = parseFloat($pixels.val());
+            if (!isNaN(px)) {
+                $rems.val(trimZeros(pxToRem(px, base)));
+            }
+        } else if (lastEdited === 'rem') {
+            const rem = parseFloat($rems.val());
+            if (!isNaN(rem)) {
+                $pixels.val(Math.round(remToPx(rem, base)));
+            }
+        }
+    });
+
+    $pixels.on('focus', function() {
+        lastEdited = 'px';
+    });
+
+    $pixels.on('keyup', function() {
+        const pxVal = $(this).val();
+        
+        if (pxVal !== '') {
+            const px = parseFloat(pxVal);
+            const base = parseFloat($base.val()) || 16;
+        
+            if (!isNaN(px)) {
+                $rems.val(trimZeros(pxToRem(px, base)));
+            }
+        }
+    });
+
+    $rems.on('focus', function() {
+        lastEdited = 'rem';
+    });
+
+    $rems.on('keyup', function() {
+        const remVal = $(this).val();
+        
+        if (remVal !== '') {
+            const rem = parseFloat(remVal);
+            const base = parseFloat($base.val()) || 16;
+
+            if (!isNaN(rem)) {
+                $pixels.val(Math.round(remToPx(rem, base)));
+            }
+        }
+    });
+})(jQuery);
---
+++ js/ratios.js
@@ -0,0 +1,89 @@
+(function($) {
+
+    function initForms() {
+        var inputs = $("input");
+
+        inputs.each(function() {
+            var $input = $(this);
+            $input.attr('data-placeholder', $input.attr('placeholder'));
+        });
+
+        inputs.on('focus', function() {
+            $(this).attr('placeholder', '');
+        }).on('blur', function() {
+            $(this).attr('placeholder', $(this).attr('data-placeholder'));
+        });
+    }
+
+    initForms();
+    
+    const $ratio = $("[id=ratio]");
+    const $width = $("[id=width]");
+    const $height = $("[id=height]");
+
+    $width.val(1440);
+    
+    function getRatioValues() {
+        const ratio = $ratio.val().split(':');
+        return {
+            widthRatio: parseFloat(ratio[0]),
+            heightRatio: parseFloat(ratio[1])
+        };
+    }
+
+    function formatValue(value) {
+        if (Math.floor(value) === value) {
+            return value.toString();
+        } else {
+            return value.toFixed(2);
+        }
+    }
+
+    function calculateHeightFromWidth() {
+        const width = parseFloat($('#width').val());
+        const { widthRatio, heightRatio } = getRatioValues();
+
+        if (!isNaN(width) && width > 0) {
+            const calculatedHeight = (width * heightRatio) / widthRatio;
+            $height.val(formatValue(calculatedHeight));
+        }
+    }
+
+    function calculateWidthFromHeight() {
+        const height = parseFloat($('#height').val());
+        const { widthRatio, heightRatio } = getRatioValues();
+
+        if (!isNaN(height) && height > 0) {
+            const calculatedWidth = (height * widthRatio) / heightRatio;
+            $width.val(formatValue(calculatedWidth));
+        }
+    }
+
+    function debounce(func, delay) {
+        let timer;
+        return function() {
+            clearTimeout(timer);
+            timer = setTimeout(func, delay);
+        };
+    }
+
+    const debouncedCalculateHeight = debounce(calculateHeightFromWidth, 300);
+    $width.on('input', function() {
+        if ($(this).val().trim() !== '') {
+            debouncedCalculateHeight();
+        }
+    });
+
+    const debouncedCalculateWidth = debounce(calculateWidthFromHeight, 300);
+    $height.on('input', function() {
+        if ($(this).val().trim() !== '') {
+            debouncedCalculateWidth();
+        }
+    });
+
+    $ratio.on('change', function() {
+        calculateHeightFromWidth();
+    });
+
+    calculateHeightFromWidth();
+})(jQuery);
